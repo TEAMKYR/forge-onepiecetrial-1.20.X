8302795a5cc654b13c1a189f7b79e374f00d00b8
TEAMKYR <ameya.m.bharadwaj@gmail.com>
2023-08-10 20:08:15 -0700

    added infrastructure for haki added devil fruit loot tables added infrastructure for sinking

--- src/main/java/net/teamkyr/onepiecemod/fruitpowers/PlayerPowers.java
+++ src/main/java/net/teamkyr/onepiecemod/fruitpowers/PlayerPowers.java
@@ -4,6 +4,8 @@ import net.minecraft.nbt.CompoundTag;
 
 public class PlayerPowers {
     private int fruits;
+    private boolean zoan = false;
+    private String mob = "";
 
     public int getFruits(){
         return fruits;
@@ -19,16 +21,24 @@ public class PlayerPowers {
 
     public void fruitReset(){
         this.fruits =0;
+        this.zoan = false;
+        this.mob = "";
     }
     public void copyFrom(PlayerPowers source){
         this.fruits = source.fruits;
     }
-
     public void saveNBTData(CompoundTag nbt){
         nbt.putInt("fruits", fruits);
     }
 
     public void loadNBTData(CompoundTag nbt){
         fruits = nbt.getInt("fruits");
+    }
+    public String getMob(){return this.mob;}
+    public boolean isZoan(){
+        return this.zoan;
+    }
+    public void setMob(String Mobname){
+        this.mob = Mobname;
     }
 }
--- src/main/java/net/teamkyr/onepiecemod/networking/packet/ExampleC2SPacket.java
+++ src/main/java/net/teamkyr/onepiecemod/networking/packet/ExampleC2SPacket.java
@@ -3,7 +3,10 @@ package net.teamkyr.onepiecemod.networking.packet;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.network.NetworkEvent;
+import net.teamkyr.onepiecemod.fruitpowers.PlayerPowersProvider;
 
 import java.util.function.Supplier;
 
@@ -27,10 +30,20 @@ public class ExampleC2SPacket {
             ServerPlayer player = context.getSender();
             ServerLevel level = player.serverLevel();
 
+            if(hasWaterAroundThem(player, level, 2) && player.getCapability(PlayerPowersProvider.PLAYER_POWERS).isPresent()){
+                player.setDeltaMovement(player.getDeltaMovement().add(0.0D, (double)-0.04F * player.getAttributeValue(ForgeMod.SWIM_SPEED.get()), 0.0D));
+                player.setAirSupply(0);
+            }
+
 
 
         });
         return true;
+    }
+
+    private boolean hasWaterAroundThem(ServerPlayer player, ServerLevel level, int size){
+        return level.getBlockStates(player.getBoundingBox().inflate(size))
+                .filter(state -> state.is(Blocks.WATER)).toArray().length > 0;
     }
 
 }
